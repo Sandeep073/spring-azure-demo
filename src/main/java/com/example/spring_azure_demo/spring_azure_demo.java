7c56bc6599e303ba43e38029b019c13ac0b96a4d
Sandeep073 <Sandeepbubby073@gmail.com>
2024-11-20 00:36:53 -0500

    third commit

---
+++ src/main/java/com/example/spring_azure_demo/spring_azure_demo.java
@@ -0,0 +1,23 @@
+package com.example.spring_azure_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class spring_azure_demo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(spring_azure_demo.class, args);
+	}
+
+	@GetMapping("/api/demo")
+	public List<String> getDemoData() {
+		return Arrays.asList("Sample Data 1", "Sample Data 2", "Sample Data 3");
+	}
+}
